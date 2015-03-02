bceb2a6e0e94abf62b1be8d87982e2cf3e82dcbd
Alex Sky <aminnsmirnoff@gmail.com>
2015-03-02 14:21:13 +0200

    change pagination

--- app/controllers/FrontController.php
+++ app/controllers/FrontController.php
@@ -46,7 +46,7 @@ class FrontController extends BaseController {
 		}
 
 		$worldNews 		= $this->getChildren('world_news',4);
-		$importantNews 	= $this->getChildren('important_news',10);	
+		$importantNews 	= $this->getChildren('important_news',5);	
 
 		$articles = $model->getLastarticles();
 		$seoData = $this->seo;	
