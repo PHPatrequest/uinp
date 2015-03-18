aeea7090237a249cb23e49f39f7ee21482231ea8
Alex Sky <aminnsmirnoff@gmail.com>
2015-03-18 18:19:36 +0200

    visual fixes

--- app/views/content/front/folder.blade.php
+++ app/views/content/front/folder.blade.php
@@ -13,35 +13,36 @@
 						<h1>{{ $item->title }}</h1>
 					</div>
 				</div>
-			@endif	
-			@if (count($folders))
-				@if(!empty($item->text))
-					<div class="row"> 
-						<div class="col-md-12 text-justify folder-desc">
-							{{ $item->text }}
-						</div>
-					</div>
-				@endif
-				@foreach ($folders as $folder)
-					<div class="col-md-12">
-						@include('content.front.folderpreview')
-					</div>
-				@endforeach
-				{{ $folders->links() }}
-			@elseif (count($children))
+			@endif
+
+			@if (count($children))
 				@foreach ($children as $article)
 					<div class="col-md-12">
 						@include('content.front.articlepreview')
 					</div>
 				@endforeach
-				{{ $children->links() }}
-				@if(!empty($item->text))
-					<div class="row"> 
-						<div class="col-md-12 text-justify folder-desc">
-							{{ $item->text }}
-						</div>
+				<div class="col-md-12">
+					{{ $children->links() }}
+				</div>
+			@endif
+
+			@if(isset($item->text) && !empty($item->text))
+				<div class="row"> 
+					<div class="col-md-12 text-justify folder-desc">
+						{{ $item->text }}
 					</div>
-				@endif
+				</div>
+			@endif
+
+			@if (count($folders))
+				@foreach ($folders as $folder)
+					<div class="col-md-12">
+						@include('content.front.folderpreview')
+					</div>
+				@endforeach
+				<div class="col-md-12">
+					{{ $folders->links() }}
+				</div>
 			@endif
 		</div>
 		<div class="col-md-4">
