e92e9f7d85ea1e21690429fba45a9a6fd7fc6f22
sky <sky@sky-Inspiron-N5110.(none)>
2015-04-27 09:49:29 +0300

    counters

--- app/views/containers/frontend.blade.php
+++ app/views/containers/frontend.blade.php
@@ -35,6 +35,7 @@
 				</div>
 				<div class="pull-left phone" style="margin: 4px 0 0 15px; text-decoration:none;">телефон редакции: <a href="tel:380443922975" style="text-decoration:none;">+38 (044) 392-29-75</a></div>	
 			</div>
+			
 			<div class="col-md-3">	
 				<div class="search">
 					<form action="/">
@@ -50,7 +51,7 @@
 					<div class="pull-left"><img src="/assets/images/uinplogo.png" style="width: 160px; margin: 20px 0 0 10px;"></div>
 					<div class="pull-left header-text" style="font-family:impact; font-size:30px; padding:5px 0 0 20px">
 						<div>украинский независимый новостной портал</div>
-						<div style="color: rgb(200, 200, 200);">ukrainian independent news portal</div>
+						<div style="color: rgb(200, 200, 200); text-align:left">ukrainian independent news portal</div>
 					</div>
 					<div style="clear:both"></div>
 				</a>
@@ -82,7 +83,73 @@
 		</div>			
 		<div class="bottom">
 			© Copyriht 2015
-		</div> 
+		</div>
+		<div style="text-align:center">
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript">
+			(function (d, w, c) {
+				(w[c] = w[c] || []).push(function() {
+					try {
+						w.yaCounter29837199 = new Ya.Metrika({
+							id:29837199,
+							clickmap:true,
+							trackLinks:true,
+							accurateTrackBounce:true
+						});
+					} catch(e) { }
+				});
+
+				var n = d.getElementsByTagName("script")[0],
+					s = d.createElement("script"),
+					f = function () { n.parentNode.insertBefore(s, n); };
+				s.type = "text/javascript";
+				s.async = true;
+				s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+				if (w.opera == "[object Opera]") {
+					d.addEventListener("DOMContentLoaded", f, false);
+				} else { f(); }
+			})(document, window, "yandex_metrika_callbacks");
+		</script>
+		<noscript><div><img src="//mc.yandex.ru/watch/29837199" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<!-- /Yandex.Metrika counter -->
+		</div>
+		<div style="text-align:center">
+				<!--LiveInternet counter--><script type="text/javascript"><!--
+		document.write("<a href='//www.liveinternet.ru/click' "+
+		"target=_blank><img src='//counter.yadro.ru/hit?t44.6;r"+
+		escape(document.referrer)+((typeof(screen)=="undefined")?"":
+		";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+		screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+		";"+Math.random()+
+		"' alt='' title='LiveInternet' "+
+		"border='0' width='31' height='31'><\/a>")
+		//--></script><!--/LiveInternet-->
+		<!-- Rating@Mail.ru counter -->
+		<script type="text/javascript">
+		var _tmr = _tmr || [];
+		_tmr.push({id: "2648013", type: "pageView", start: (new Date()).getTime()});
+		(function (d, w) {
+		   var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true;
+		   ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+		   var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+		   if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+		})(document, window);
+		</script><noscript><div style="position:absolute;left:-10000px;">
+		<img src="//top-fwz1.mail.ru/counter?id=2648013;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
+		</div></noscript>
+		<!-- //Rating@Mail.ru counter -->
+		
+		<!-- begin of Top100 code -->
+		<script id="top100Counter" type="text/javascript" src="http://counter.rambler.ru/top100.jcn?3103915"></script>
+		<noscript>
+		<a href="http://top100.rambler.ru/navi/3103915/">
+		<img src="http://counter.rambler.ru/top100.cnt?3103915" alt="Rambler's Top100" border="0" />
+		</a>
+
+		</noscript>
+		<!-- end of Top100 code -->
+		</div>
 	</div>	
 </body>
 </html>
