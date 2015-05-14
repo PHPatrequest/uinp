b572f17926805e81a385a55b08cbbe2fac7c0d8a
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-14 06:04:32 +0300

    test

--- app/views/content/front/articlepreview.blade.php
+++ app/views/content/front/articlepreview.blade.php
@@ -3,9 +3,9 @@
 		<div class="col-md-12" style="overflow: hidden;">
 			<div style="display:none">{{ $article->id }}</div>
 			@if(!empty($article->thumb))
-				<!-- <img src="/{{ $article->thumb }}" class="img-thumbnail pull-left" property="image"> -->
+				<img src="/{{ $article->thumb }}" class="img-thumbnail pull-left" property="image">
 			@elseif(!empty($article->video))
-				<!-- <img src="http://img.youtube.com/vi/{{ $article->video }}/0.jpg" class="img-thumbnail pull-left" property="image"> -->
+				<img src="http://img.youtube.com/vi/{{ $article->video }}/0.jpg" class="img-thumbnail pull-left" property="image">
 			@endif
 			<div style="overflow:hidden">
 				<h2 property="name"><a href="/{{ $article->path }}/{{ $article->alias }}">{{ $article->title }}</a></h2>			
