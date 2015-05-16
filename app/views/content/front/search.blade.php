92ed03cbf2d183d2db357d500069705d7b8733c5
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-16 11:39:48 +0300

    test

--- app/views/content/front/search.blade.php
+++ app/views/content/front/search.blade.php
@@ -1,22 +1,10 @@
 @extends('containers.frontend')
 @section('title')Поиск: {{ $search }}@stop
 @section('main')
-<?php exit('123'); ?>
 	<div class="row">
 		<div class="col-md-8">
 			<h1 class="text-center">вы искали: {{ $search }}</h1>	
-			@if (count($articles))
-				@foreach ($articles as $article)
-					<div class="col-md-12">
-						@include('content.front.articlepreview')
-					</div>
-				@endforeach
-				<div class="col-md-12">
-					{{ $articles->links() }}
-				</div>
-			@else
-				<h4 class="text-center">Ничего не найдено</h4>
-			@endif
+
 		</div>
 		<div class="col-md-4">
 			@include('content.front.rightblock')
