98ef9890065844876c3729eb1360908c594eb715
root <root@vps324.stabline.net>
2015-07-05 20:19:16 +0300

    redtram block

--- app/views/content/front/rightblock.blade.php
+++ app/views/content/front/rightblock.blade.php
@@ -4,6 +4,26 @@
 		@include('content.front.articlelistitem')
 	@endforeach	
 @endif
+<div class="heading">Актуальные новости</div>
+<div class="video_block">
+	<div id="n4p_30655">Loading...</div>
+	<script type="text/javascript" charset="utf-8">
+	  (function(d,s){
+	    var o=d.createElement(s);
+	    o.async=true;
+	    o.type="text/javascript";
+	    o.charset="utf-8";
+	    if (location.protocol == "https:") {
+	      o.src="https://js-ua.redtram.com/n4p/0/30/ticker_30655.js";
+	    }
+	    else {
+	      o.src="http://js.ua.redtram.com/n4p/0/30/ticker_30655.js";
+	    }
+	    var x=d.getElementsByTagName(s)[0];
+	    x.parentNode.insertBefore(o,x);
+	  })(document,"script");
+	</script>
+</div>
 @if(isset($accidentNews)&&!empty($accidentNews))
 	<div class="heading">Происшествия</div>
 	@foreach($accidentNews as $article)
@@ -18,4 +38,5 @@
 		@endforeach
 	</div>
 @endif
+
 @include('content.front.socwidgets')
